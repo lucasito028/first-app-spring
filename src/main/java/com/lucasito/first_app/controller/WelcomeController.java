ce4fef698dcd6525ec239b40cc5736fa194272be
lucasito028 <lucastoshihiroito28@gmail.com>
2024-08-30 19:45:36 -0300

    Finalizando ai

--- src/main/java/com/lucasito/first_app/controller/WelcomeController.java
+++ src/main/java/com/lucasito/first_app/controller/WelcomeController.java
@@ -2,10 +2,16 @@ package com.lucasito.first_app.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucasito.first_app.domain.User;
 import com.lucasito.first_app.service.WelcomeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -21,6 +27,12 @@ public class WelcomeController {
 
     @RequestMapping("/")
     String message(){
-        return this.WelcomeService.message("Pau no Teu cu");
+        return this.WelcomeService.message("Pao e Carne");
     }
+
+    @PostMapping("/{id}")
+    String helloworldpost(@PathVariable("id") String id, @RequestParam(value = "filter", defaultValue = "none") String filter, @RequestBody User body){
+        return "Salve " + body.getName() + " " + id + " "+ filter; 
+    }
+
 }
